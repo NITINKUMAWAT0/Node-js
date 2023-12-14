d21f9ade09232f3b967f25f13578f60dba4000ec
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-12-14 10:37:47 +0530

    learning req and res in web server!!

--- index.js
+++ index.js
@@ -8,13 +8,33 @@ const jsonData = fs.readFileSync('data.json','utf-8');
 const data = { age: 5 };
 const server = http.createServer((req, res) => {
 
+    console.log(req.url);
+
+    switch(req.url){
+        case '/':
+            res.setHeader('cotent-type','text/html');
+            res.end(index);
+            break;
+
+        case '/api':
+            res.setHeader('content-type','application/JSON');
+            res.end(JSON.stringify(jsonData));
+            break;
+        default :
+            res.writeHead(404,'NOT FOUND');  
+            res.end();  
+    }
+
     console.log("server started!!");
-    res.setHeader('heading', 'info');
-    res.setHeader('content-type','application/JSON');
+    
+    // res.setHeader('heading', 'info');
+
+    // res.setHeader('content-type','application/JSON');
+
     // res.setHeader('content-type','text/html');
+
     // res.end(JSON.stringify(data));
     // res.end(index);
-    res.end(jsonData);
 });
 
 
